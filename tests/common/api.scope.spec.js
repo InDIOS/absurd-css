c0e7b79dfcd1980c385b5ed6849c7b418fe0c0ed
Roberto Asiel Guevara Castañeda <raguevara@uci.cu>
2016-11-14 11:11:47 -0500

    Added more test for scope style

--- tests/common/api.scope.spec.js
+++ tests/common/api.scope.spec.js
@@ -31,7 +31,7 @@ describe("API(scope)", function () {
 		}).compile(function (err, css) {
 			expect(err).toBe(null);
 			expect(css).toBeDefined();
-			expect(css).toBe(".scoped .absurd-title{border-radius: 10px;}");
+			expect(css).toBe(".scoped.absurd-title{border-radius: 10px;}");
 			done();
 		}, { minify: true });
 	});
@@ -47,14 +47,14 @@ describe("API(scope)", function () {
 		A.compile(function (err, css) {
 			expect(err).toBe(null);
 			expect(css).toBeDefined();
-			expect(css).toBe(".scoped .absurd-title{border-radius: 10px;}");
+			expect(css).toBe(".scoped.absurd-title{border-radius: 10px;}");
 			done();
 		}, { minify: true });
 	});
 
 	it("should use scope with multiple selectors", function (done) {
 		var A = Absurd();
-		A.scope('.scoped #scope');
+		A.scope('#scope.scoped');
 		A.add({
 			'.absurd-title': {
 				'border-radius': '10px'
@@ -63,7 +63,91 @@ describe("API(scope)", function () {
 		A.compile(function (err, css) {
 			expect(err).toBe(null);
 			expect(css).toBeDefined();
-			expect(css).toBe(".scoped #scope .absurd-title{border-radius: 10px;}");
+			expect(css).toBe("#scope.scoped.absurd-title{border-radius: 10px;}");
+			done();
+		}, { minify: true });
+	});
+
+	it("should use scope in every top selectors", function (done) {
+		var A = Absurd();
+		A.scope('#scope.scoped');
+		A.add({
+			'.absurd-title': {
+				'border-radius': '10px',
+				p: {
+					color: '#865748'
+				}
+			},
+			span: {
+				mar: '4px auto'
+			}
+		});
+		A.compile(function (err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe("#scope.scoped.absurd-title{border-radius: 10px;}#scope.scoped.absurd-title p{color: #865748;}span#scope.scoped{margin: 4px auto;}");
+			done();
+		}, { minify: true });
+	});
+
+	it("should set scope depending of selector type", function (done) {
+		var A = Absurd();
+		A.scope('#scope.scoped');
+		A.add({
+			'.absurd-title': {
+				'border-radius': '10px'
+			},
+			'.absurd .title': {
+				'border-radius': '16px'
+			},
+			span: {
+				mar: '4px auto'
+			},
+			'#span': {
+				pad: '4px auto'
+			},
+			'span > a': {
+				pad: '6px auto'
+			},
+			'ul > #item > .link': {
+				pad: '8px auto'
+			},
+			'span + a': {
+				fz: '14px'
+			},
+			'p ~ span': {
+				fz: '18px'
+			}
+		});
+		A.compile(function (err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe("#scope.scoped.absurd-title{border-radius: 10px;}#scope.scoped.absurd .title{border-radius: 16px;}span#scope.scoped{margin: 4px auto;}#scope.scoped#span{padding: 4px auto;}span#scope.scoped > a#scope.scoped{padding: 6px auto;}ul#scope.scoped > #scope.scoped#item > #scope.scoped.link{padding: 8px auto;}span#scope.scoped + a#scope.scoped{font-size: 14px;}p#scope.scoped ~ span#scope.scoped{font-size: 18px;}");
+			done();
+		}, { minify: true });
+	});
+
+	it("should set scope to multiple selectors", function (done) {
+		var A = Absurd();
+		A.scope('#scope.scoped');
+		A.add({
+			'.absurd, .title': {
+				'border-radius': '16px'
+			},
+			'.text, span > a': {
+				pad: '4px auto'
+			},
+			'#text + span, a': {
+				fz: '14px'
+			},
+			'.title, p ~ span': {
+				fz: '18px'
+			}
+		});
+		A.compile(function (err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe("#scope.scoped.absurd,#scope.scoped.title{border-radius: 16px;}#scope.scoped.title,p#scope.scoped ~ span#scope.scoped{font-size: 18px;}#scope.scoped.text,span#scope.scoped > a#scope.scoped{padding: 4px auto;}#scope.scoped#text + span#scope.scoped,a#scope.scoped{font-size: 14px;}");
 			done();
 		}, { minify: true });
 	});
